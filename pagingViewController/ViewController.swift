b1743487d20243b7fca6d5acd82500edc84b2ae4
youssefes <ssh://git@my.domain.cz:2222/project-name>
2020-10-13 12:44:50 +0200

    Initial Commit

---
+++ pagingViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pagingViewController
+//
+//  Created by youssef on 10/13/20.
+//  Copyright © 2020 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
